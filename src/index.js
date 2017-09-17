8b3a05f003d3812c0595afc0b9ae78ec000a1ee3
volshibenik <volshibenik@gmail.com>
2017-09-17 15:06:48 +0300

    added factorial functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function zeros(expression) {
-  // your solution
+  expression = '';
+  console.log('aa',factorial(95)*factorial(95)*factorial(95)*factorial(95));
+}
+
+function factorial(num) {
+  return num === 1 ? num : num*factorial(num-1);
+}
+
+function doubleFactorial(num) {
+  if (num % 2) {
+    return num === 1 ? num : num * doubleFactorial(num-2);
+  } else {
+    return num === 2 ? num : num * doubleFactorial(num-2)
+  }
 }
